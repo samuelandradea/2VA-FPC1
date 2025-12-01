6b2909d7bf251ec613e7fcd7d7c0188d8dc54364
Pedro Almeida <pedroarthur.almeida@outlook.com>
2025-12-01 17:22:07 -0300

    Update projeto2.py

--- projeto2/codigo/projeto2.py
+++ projeto2/codigo/projeto2.py
@@ -12,8 +12,21 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    def insert_sorted(self, id_val):
+        new_node = Node(id_val)
+        if self.head is None or id_val < self.head.id:
+            new_node.next = self.head
+            self.head = new_node
+            return
 
-    def search_sequential(self, target):
+        current = self.head
+        while current.next and current.next.id < id_val:
+            current = current.next
+
+        new_node.next = current.next
+        current.next = new_node
+
+    def search(self, target):
         current = self.head
         while current:
             if current.id == target:
@@ -23,26 +36,18 @@ class LinkedList:
 
 
 def carregar_ids(caminho):
-    with open(caminho, "r") as f:
-        return [int(line.strip()) for line in f if line.strip()]
+    with open(caminho) as f:
+        return [int(line) for line in f if line.strip()]
 
 
-def criar_lista_encadeada_ordenada(ids):
-    ids.sort()
+def criar_lista(ids):
     lista = LinkedList()
-    current = None
-    for id_val in ids:
-        new_node = Node(id_val)
-        if lista.head is None:
-            lista.head = new_node
-            current = new_node
-        else:
-            current.next = new_node
-            current = new_node
+    for idv in ids:
+        lista.insert_sorted(idv)
     return lista
 
 
-def criar_vetor_indices(lista):
+def criar_vetor(lista):
     vetor = []
     current = lista.head
     while current:
@@ -51,103 +56,78 @@ def criar_vetor_indices(lista):
     return vetor
 
 
-def busca_binaria_vetor(vetor, alvo):
-    inicio, fim = 0, len(vetor) - 1
-    while inicio <= fim:
-        meio = (inicio + fim) // 2
+def busca_binaria(vetor, alvo):
+    ini, fim = 0, len(vetor) - 1
+    while ini <= fim:
+        meio = (ini + fim) // 2
         valor = vetor[meio][0]
         if valor == alvo:
             return 1
         if alvo < valor:
             fim = meio - 1
         else:
-            inicio = meio + 1
+            ini = meio + 1
     return -1
 
 
 class HashTable:
     def __init__(self, tamanho):
-        self.tamanho = tamanho
         self.tabela = [[] for _ in range(tamanho)]
+        self.tamanho = tamanho
 
+    def _hash(self, val):
+        return val % self.tamanho
 
-    def funcao_hash(self, id_val):
-        return id_val % self.tamanho
+    def inserir(self, val):
+        self.tabela[self._hash(val)].append(val)
 
-
-    def inserir(self, id_val):
-        pos = self.funcao_hash(id_val)
-        self.tabela[pos].append(id_val)
-
-
-    def buscar(self, id_val):
-        pos = self.funcao_hash(id_val)
-        for x in self.tabela[pos]:
-            if x == id_val:
+    def buscar(self, val):
+        for x in self.tabela[self._hash(val)]:
+            if x == val:
                 return 1
         return -1
 
 
-def tempo_lista(lista, ids):
+def medir_tempo(func, itens):
     ini = perf_counter()
-    r = [lista.search_sequential(x) for x in ids]
+    r = [func(x) for x in itens]
     fim = perf_counter()
     return r, (fim - ini) * 1000
 
 
-def tempo_binario(vetor, ids):
-    ini = perf_counter()
-    r = [busca_binaria_vetor(vetor, x) for x in ids]
-    fim = perf_counter()
-    return r, (fim - ini) * 1000
-
-
-def tempo_hash(tabela, ids):
-    ini = perf_counter()
-    r = [tabela.buscar(x) for x in ids]
-    fim = perf_counter()
-    return r, (fim - ini) * 1000
-
-
-def gerar_grafico(t1, t2, t3, caminho):
-    nomes = ["Sequencial", "Binária", "Hash"]
-    tempos = [t1, t2, t3]
-
-    plt.figure(figsize=(7,5))
-    plt.bar(nomes, tempos)
+def grafico(t1, t2, t3, caminho):
+    plt.bar(["Sequencial", "Binária", "Hash"], [t1, t2, t3])
     plt.yscale("log")
-    plt.ylabel("Tempo (ms) - escala logarítmica")
-    plt.title("Comparação de desempenho - Projeto 2")
-    plt.tight_layout()
+    plt.title("Projeto 2 - comparação")
+    plt.ylabel("ms")
     plt.savefig(caminho)
     plt.close()
 
 
 if __name__ == "__main__":
-    base = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-    dados_dir = os.path.join(base, "dados")
-    grafico_dir = os.path.join(base, "grafico")
-    os.makedirs(grafico_dir, exist_ok=True)
 
-    entrada = os.path.join(dados_dir, "projeto_2_lista_IDs_entrada.txt")
-    busca = os.path.join(dados_dir, "projeto_2_lista_IDs_busca.txt")
+    base = os.path.abspath(os.path.dirname(__file__))
+    dados = os.path.join(base, "..", "dados")
+    graf = os.path.join(base, "..", "grafico")
 
-    ids_entrada = carregar_ids(entrada)
-    ids_busca = carregar_ids(busca)
+    os.makedirs(graf, exist_ok=True)
 
-    lista = criar_lista_encadeada_ordenada(ids_entrada)
-    vetor = criar_vetor_indices(lista)
+    ids = carregar_ids(os.path.join(dados, "projeto_2_lista_IDs_entrada.txt"))
+    busca = carregar_ids(os.path.join(dados, "projeto_2_lista_IDs_busca.txt"))
 
-    tabela = HashTable(400000)
-    for x in ids_entrada:
+    lista = criar_lista(ids)
+    vetor = criar_vetor(lista)
+
+    tabela = HashTable(7000)
+    for x in ids:
         tabela.inserir(x)
 
-    res_seq, t_seq = tempo_lista(lista, ids_busca)
-    res_bin, t_bin = tempo_binario(vetor, ids_busca)
-    res_hash, t_hash = tempo_hash(tabela, ids_busca)
+    _, t_seq = medir_tempo(lista.search, busca)
+    _, t_bin = medir_tempo(lambda x: busca_binaria(vetor, x), busca)
+    _, t_hash = medir_tempo(tabela.buscar, busca)
 
-    gerar_grafico(t_seq, t_bin, t_hash, os.path.join(grafico_dir, "projeto2_comparacao.png"))
+    grafico(t_seq, t_bin, t_hash, os.path.join(graf, "comparacao2.png"))
 
-    print("Tempo Sequencial (ms):", t_seq)
-    print("Tempo Binária (ms):", t_bin)
-    print("Tempo Hash (ms):", t_hash)
+    print("Sequencial:", t_seq)
+    print("Binária:", t_bin)
+    print("Hash:", t_hash)
